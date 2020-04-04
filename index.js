63f39e5193ac65cd28e3acc29168537dfd4fa579
hangoo <imhangoo@gmail.com>
2020-04-03 18:57:57 -0700

    sdf

--- index.js
+++ index.js
@@ -7,39 +7,41 @@ const puppeteer = require('puppeteer');
 const nodemailer = require('nodemailer');
 
 const transport = nodemailer.createTransport({
-  service: 'yahoo',
+  service: 'qq',
   auth: {
-    user: 'imhangoo@yahoo.com',
-    pass: 'ykisegolslbdbpra'
+    user: 'zaibietonghua@qq.com',
+    pass: 'swnykznnanftcabb'
   }
 });
 
-const bestbuy = 'https://www.bestbuy.ca/en-ca/product/nintendo-switch-console-with-neon-red-blue-joy-con/13817625';
-const walmart = 'https://www.walmart.ca/en/ip/nintendo-switch-with-neon-blue-and-neon-red-joycon-nintendo-switch/6000200280557';
+const bestbuyLink = 'https://www.bestbuy.ca/en-ca/product/nintendo-switch-console-with-neon-red-blue-joy-con/13817625';
+const walmartLink = 'https://www.walmart.ca/en/ip/nintendo-switch-with-neon-blue-and-neon-red-joycon-nintendo-switch/6000200280557';
 
 const stores = {
-  bestbuy: bestbuy,
-  walmart: walmart
+  bestbuy: bestbuyLink,
+  walmart: walmartLink
 }
 
-function send(store) {
+function send(storeName) {
   const message = {
-    from: 'imhangoo@yahoo.com', // Sender address
+    from: 'zaibietonghua@qq.com',// Sender address
     to: '344375048@qq.com',         // List of recipients
-    subject: store + ': Nintendo Switch Available', // Subject line
-    text: stores[store] // Plain text body
+    subject: storeName + ': Nintendo Switch Available', // Subject line
+    text: storeName +  ' ' + getDate(), // Plain text body
+    html: `<a href="${stores[storeName]}"> ${storeName} Switch</a>`
+
   };
   transport.sendMail(message, function (err, info) {
     if (err) {
       console.log(err)
     } else {
-      console.log(info);
+      console.log(`${storeName} has stocks, email sent.`);
     }
   });
 }
 
 (async () => {
-  const browser = await puppeteer.launch()
+  const browser = await puppeteer.launch();
   const page = await browser.newPage()
   await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36");
   await page.setViewport({ width: 1280, height: 800 })
@@ -47,16 +49,16 @@ function send(store) {
   while (true) {
     try {
       // best buy
-      await page.goto(bestbuy)
-      let available = await page.$$eval('.addToCartButton', anchors => { return !anchors[0].hasAttribute('disabled'); });
-      if (!available) {
+      await page.goto(stores.bestbuy)
+      const available = await page.$$eval('.addToCartButton', anchors => { return !anchors[0].hasAttribute('disabled'); });
+      if (available) {
         send('bestbuy');
       } else {
         console.log('Bestbuy out of stock at ' + getDate());
       }
 
       // walmart
-      await page.goto(walmart, { waitUntil: 'networkidle2' })
+      await page.goto(stores.walmart, { waitUntil: 'networkidle2' })
       const elementHandle = await page.$("button[data-automation='cta-button']");
       const text = await page.evaluate(el => el.textContent, elementHandle);
       if (text == 'Add to cart') {
@@ -64,7 +66,7 @@ function send(store) {
       } else {
         console.log('Walmart out of stock at ' + getDate());
       }
-      const nap = between(10, 40);
+      const nap = between(5, 10);
       await sleep(nap * 1000);
     }
     catch (err) {
